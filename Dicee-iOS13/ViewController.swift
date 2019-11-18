c0a5faf3475f6302528a6db5a54616838f29ec93
oscargrp <oscargrp@gmail.com>
2019-11-17 22:30:05 -0800

    Dice app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Remember to use "refactor" to rename
+    // IBOulet allows me to reference a UI element
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // To create a random number you can use Int.random(in: 0...5)
+        leftDiceImageView.image = diceArray.randomElement()
+        rightDiceImageView.image = diceArray.randomElement()
+    }
 }
 
